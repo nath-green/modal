97adea1d4dfeaba57ca9b47c8c23bb587381014d
Nathan Green <nathan.green@jet2.com>
2018-08-08 14:28:46 +0100

    change to class pattern

--- src/index.js
+++ src/index.js
@@ -1,36 +1,29 @@
 import $ from 'jquery'
 import { stateToggler } from 'state-toggler'
 
-export const Modal = (function () {
-  // Bind click on modal-action on import
-  $('[modal-action]').click(function () {
-    let action = $(this).attr('modal-action')
-    // if target is not set on modal, get target from parent
-    let target = $(this).attr('modal-target') || $(this).closest('.modal').attr('modal')
-    Modal[action](target)
-  })
+export class Modal {
+  static init () {
+    $('[modal-action]').click(function () {
+      let action = $(this).attr('modal-action')
+      // if target is not set on modal, get target from parent
+      let target = $(this).attr('modal-target') || $(this).closest('.modal').attr('modal')
+      Modal[action](target)
+    })
+  }
 
-  const open = target => {
+  static open (target) {
     $(`[modal="${target}"]`).attr('modal-state', 'open')
   }
 
-  const close = target => {
+  static close (target) {
     $(`[modal="${target}"]`).attr('modal-state', 'closed')
   }
 
-  const closeAll = () => {
+  static closeAll () {
     $('[modal]').attr('modal-state', 'closed')
   }
 
-  const toggle = target => {
+  static toggle (target) {
     stateToggler(`[modal="${target}"]`, {attr: 'modal-state'})
   }
-
-  // Public API for modal actions not click bound
-  return {
-    open: open,
-    close: close,
-    closeAll: closeAll,
-    toggle: toggle
-  }
-})()
+}
